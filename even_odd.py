37c73f743134e27fec3977efd6f2ee6dcc700ba2
SABBELLA KAVYA SUVARNIKA <kavyasabbella246@gmail.com>
2026-07-29 13:35:08 +0530

    Improve even odd program with validation and comments

--- even_odd.py
+++ even_odd.py
@@ -1,6 +1,16 @@
-num = int(input("Enter a number: "))
+# Program: Even or Odd Number
+# Description: This program checks whether the entered integer is even or odd.
 
-if num % 2 == 0:
-    print(num, "is an Even Number")
-else:
-    print(num, "is an Odd Number")
+try:
+    # Take an integer from the user
+    num = int(input("Enter an integer: "))
+
+    # Check whether the number is divisible by 2
+    if num % 2 == 0:
+        print(num, "is an Even Number")
+    else:
+        print(num, "is an Odd Number")
+
+except ValueError:
+    # Handle input that is not an integer
+    print("Invalid input! Please enter a valid integer.")
